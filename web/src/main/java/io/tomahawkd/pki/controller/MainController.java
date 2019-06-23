bb9ed12757970914bf602ddc6286f346fdae1ffd
czzzzz0 <524186572@qq.com>
2019-06-23 15:31:45 +0800

    User Register and Login Controller

--- web/src/main/java/io/tomahawkd/pki/controller/MainController.java
+++ web/src/main/java/io/tomahawkd/pki/controller/MainController.java
@@ -1,13 +1,22 @@
 package io.tomahawkd.pki.controller;
 
+import io.tomahawkd.pki.model.SystemLogModel;
+import io.tomahawkd.pki.service.SystemLogService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class MainController {
+    @Resource
+    private SystemLogService service;
 
-	@GetMapping("/")
-	public String hello() {
-		return "hello";
-	}
+    @GetMapping("/")
+    public String hello() {
+
+        service.insertLogRecord("b","add",SystemLogModel.INFO,"fjdhf");
+
+        return "hello";
+    }
 }
