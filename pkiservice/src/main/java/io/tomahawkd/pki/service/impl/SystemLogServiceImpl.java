0d8784206680adccb4734eb443ffcf9f51bb1cf9
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-06-24 00:19:08 +0800

    delete unused

--- pkiservice/src/main/java/io/tomahawkd/pki/service/impl/SystemLogServiceImpl.java
+++ pkiservice/src/main/java/io/tomahawkd/pki/service/impl/SystemLogServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
-@Service()
+@Service
 @Transactional(rollbackFor = Exception.class)
 public class SystemLogServiceImpl implements SystemLogService {
 
