9bd7d1b8303c82e5fe085c9b4b7fda851b59dc61
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-06-23 23:43:10 +0800

    asymmetric keys generation service

--- web/src/main/java/io/tomahawkd/pki/service/UserKeyService.java
+++ web/src/main/java/io/tomahawkd/pki/service/UserKeyService.java
@@ -1,12 +1,11 @@
 package io.tomahawkd.pki.service;
 
+import io.tomahawkd.pki.exceptions.api.CipherErrorException;
 import io.tomahawkd.pki.model.UserKeyModel;
-
-import java.security.NoSuchAlgorithmException;
 
 public interface UserKeyService {
 
-	void generateKeysFor(int userId, int systemId) throws NoSuchAlgorithmException;
+	public String createKeyForm(int userId, int systemId, String random) throws CipherErrorException;
 
-	UserKeyModel getKeyById(int userId, int systemId);
+	UserKeyModel getKeyFormById(int userId, int systemId);
 }
