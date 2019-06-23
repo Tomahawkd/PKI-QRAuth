3b1986eb9aeaf961238140fa66e9c668050ba505
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-06-24 00:20:32 +0800

    move to father dir

--- web/src/main/java/io/tomahawkd/simpleserver/service/base/UserInfoService.java
+++
@@ -1,8 +0,0 @@
-package io.tomahawkd.simpleserver.service.base;
-
-import io.tomahawkd.simpleserver.model.base.UserInfoModel;
-
-public interface UserInfoService {
-    UserInfoModel getUserInfo(String username);
-    boolean changeUserInfo(UserInfoModel model) throws Exception;
-}
