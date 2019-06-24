d44ee966b59eaf75baa42633652e9faac6ea1de8
zhangxu1814 <1304690116@qq.com>
2019-06-24 14:11:58 +0800

    test

--- android/app/src/main/java/com/Vshows/PKI/Login.java
+++ android/app/src/main/java/com/Vshows/PKI/Login.java
@@ -3,12 +3,26 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Login extends AppCompatActivity implements View.OnClickListener  {
 
@@ -18,6 +32,12 @@ public class Login extends AppCompatActivity implements View.OnClickListener  {
     private TextView forget;
     private ImageButton login;
     private ImageButton showBtn;
+
+    private String name ;
+    private String psw ;
+    String TAG = Register.class.getCanonicalName();
+
+    private JSONObject jsonObject;
 
 
 
@@ -30,8 +50,8 @@ public class Login extends AppCompatActivity implements View.OnClickListener  {
 
     }
     private void initView() {
-        username = findViewById(R.id.username_re);
-        password = findViewById(R.id.password_re);
+        username = (EditText)findViewById(R.id.username);
+        password =(EditText) findViewById(R.id.password);
         register =(TextView)findViewById(R.id.register);
         forget = (TextView) findViewById(R.id.forget);
         forget.setOnClickListener(this);
@@ -51,11 +71,97 @@ public class Login extends AppCompatActivity implements View.OnClickListener  {
 
                 break;
             case R.id.loginBtn:
-                Intent intent2 = new Intent(this,index.class);
-                startActivity(intent2);
+                name = username.getText().toString();
+                psw = password.getText().toString();
+
+                if(TextUtils.isEmpty(name))
+                    Toast.makeText(this,"请输入用户名！", Toast.LENGTH_LONG).show();
+                else if (TextUtils.isEmpty(psw))
+                    Toast.makeText(this,"请输入密码！", Toast.LENGTH_LONG).show();
+                else {
+                    System.out.println("---------------------------------------------------------");
+                    registerPOST(login);
+                    Toast.makeText(this, "登录成功！", Toast.LENGTH_LONG).show();
+                    Intent intent2 = new Intent(this,index.class);
+                    startActivity(intent2);
+                }
+
                 break;
             default:
 
+        }
+    }
+
+    public void registerPOST(View view) {
+        try{
+            jsonObject = new JSONObject();
+            jsonObject.put("username",name);
+            jsonObject.put("password",psw);
+
+            new Thread(postRun).start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    Runnable postRun = new Runnable() {
+        @Override
+        public void run() {
+            requestPost(jsonObject);
+        }
+    };
+
+    private void requestPost(JSONObject jsonObject) {
+        try {
+            String path = "https://www.baidu.com";
+            String content = String.valueOf(jsonObject);
+            Log.e(TAG,"params-post-->" + content);
+            System.out.println(jsonObject);
+
+            URL url = new URL(path);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setUseCaches(false);
+            connection.setInstanceFollowRedirects(true);
+            connection.connect();
+
+            PrintWriter printWriter = new PrintWriter(connection.getOutputStream());
+            printWriter.write(content);
+            printWriter.flush();
+            printWriter.close();
+
+            if (connection.getResponseCode() == 200){
+                String result = streamToString(connection.getInputStream());
+                Log.e(TAG, "Post方式请求成功，result--->" + result);
+            }else {
+                Log.e(TAG, "Post方式请求失败");
+            }
+
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String streamToString(InputStream inputStream){
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            while ((len = inputStream.read(buffer)) != -1) {
+                baos.write(buffer, 0, len);
+            }
+            baos.close();
+            inputStream.close();
+            byte[] byteArray = baos.toByteArray();
+            return new String(byteArray);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
