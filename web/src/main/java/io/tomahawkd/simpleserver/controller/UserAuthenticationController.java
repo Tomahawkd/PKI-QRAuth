8e9f82cf45b6a98fd63267296216839925d75d3c
czzzzz0 <524186572@qq.com>
2019-06-26 09:42:52 +0800

    sesson 1 user info get update use password update

--- web/src/main/java/io/tomahawkd/simpleserver/controller/UserAuthenticationController.java
+++ web/src/main/java/io/tomahawkd/simpleserver/controller/UserAuthenticationController.java
@@ -6,12 +6,14 @@ import io.tomahawkd.simpleserver.model.SystemLogModel;
 import io.tomahawkd.simpleserver.model.UserPasswordModel;
 import io.tomahawkd.simpleserver.service.SystemLogService;
 import io.tomahawkd.simpleserver.service.UserPasswordService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.Map;
 
 @RestController
@@ -22,16 +24,19 @@ public class UserAuthenticationController {
 	private UserPasswordService userPasswordService;
 	@Resource
 	private SystemLogService systemLogService;
+	@Autowired
+	private StringRedisTemplate redisTemplate;
 
 	@PostMapping("/register")
-	public String registerUser(@RequestBody String body) {
+	public String userRegister(HttpServletRequest request, String body) {
 
 		Map<String, String> bodyData =
-				new Gson().fromJson(body, new TypeToken<Map<String, String>>() {}.getType());
+				new Gson().fromJson(body, new TypeToken<Map<String, String>>() {
+				}.getType());
 
 		String username = bodyData.get("username");
 		String password = bodyData.get("password");
-
+System.out.println(username);
 		systemLogService.insertLogRecord(UserAuthenticationController.class.getName(),
 				"registerUser", SystemLogModel.DEBUG,
 				"checkUserExistence: " + username);
@@ -39,7 +44,7 @@ public class UserAuthenticationController {
 		if (userPasswordService.checkUserExistence(username)) {
 			systemLogService.insertLogRecord(UserAuthenticationController.class.getName(),
 					"registerUser", SystemLogModel.WARN, "user existed: " + username);
-			return "{\"status\": 1, \"message\": \"user already exist\"}";
+			return "{\"status\": -1, \"message\": \"user already existing\"}";
 		}
 
 		systemLogService.insertLogRecord(UserAuthenticationController.class.getName(),
@@ -53,7 +58,7 @@ public class UserAuthenticationController {
 				"{\"status\": 0, \"message\": \"success\"}" :
 				"{\"status\": 1, \"message\": \"failed\"}";
 	}
-
+/*
 	@PostMapping("/login")
 	public String userLogin(@RequestBody String user) {
 
@@ -82,5 +87,22 @@ public class UserAuthenticationController {
 					SystemLogModel.WARN, "User password wrong");
 			return "{\"status\": 1, \"message\": \"password incorrect\"}";
 		}
+	}*/
+
+	@PostMapping(value = "/login")
+	public String userLogin(@RequestParam("userName") String username, @RequestParam("password") String password,
+						 HttpServletRequest request, HttpServletResponse response ) throws Exception {
+		response.setContentType("text/html;charset=utf-8");
+		response.setCharacterEncoding("utf-8");
+		if (!userPasswordService.checkUserExistence(username)) {      //用户不存在
+			systemLogService.insertLogRecord(UserAuthenticationController.class.getName(), "userLogin",
+					SystemLogModel.WARN, "User " + username + " not exist");
+			return "{\"status\": -1, \"message\": \"user not exist\"}";
+		}
+		if (userPasswordService.checkPassword(username, password)) {  //登陆成功
+			request.getSession().setAttribute("users", username);//用户名存入该用户的session 中
+			return "{\"status\": 0, \"message\": \"success\"}";
+		} else
+			return "{\"status\": 1, \"message\": \"password incorrect\"}";
 	}
 }
