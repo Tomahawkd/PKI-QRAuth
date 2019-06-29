bc417f93f630841d2054fe77e751d341bab33523
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-06-29 16:13:33 +0800

    add convenience function

--- pkiservice/src/main/java/io/tomahawkd/pki/service/SystemLogService.java
+++ pkiservice/src/main/java/io/tomahawkd/pki/service/SystemLogService.java
@@ -1,5 +1,10 @@
 package io.tomahawkd.pki.service;
 
+import javax.servlet.http.HttpServletRequest;
+
 public interface SystemLogService {
-    public boolean insertLogRecord(String module, String function, int level, String message);
+
+    boolean insertLogRecord(String module, String function, int level, String message);
+
+    void addAccessLog(String module, String function, String ip, String ua);
 }
