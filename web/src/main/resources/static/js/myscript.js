b8334033a2db9616cce58e2d684e7de17d5e01ff
Dracula1998 <1446953337@qq.com>
2019-06-30 00:43:31 +0800

    add a image area

--- web/src/main/resources/static/js/myscript.js
+++ web/src/main/resources/static/js/myscript.js
@@ -25,4 +25,19 @@ $(document).ready(function () {
         $('#log-menu-item').addClass('active');
         $('.content-inner').load('log.html');
     });
+
+    //选择图片，马上预览
+     function xmTanUploadImg(obj) {
+      var file = obj.files[0];
+      console.log(obj);console.log(file);
+      console.log("file.size = " + file.size);
+      var reader = new FileReader();
+      reader.onload = function (e) {
+          console.log("成功读取....");
+      var img = document.getElementById("avarimgs");
+          img.src = e.target.result;
+       //或者 img.src = this.result;  //e.target == this
+      }
+          reader.readAsDataURL(file)
+      }
 });
