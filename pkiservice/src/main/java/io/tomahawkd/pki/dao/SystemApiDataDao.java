39aae530f7d4ad13364e463a6486c7a58c8b3b65
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-06-29 19:10:13 +0800

    change to match db table

--- pkiservice/src/main/java/io/tomahawkd/pki/dao/SystemApiDataDao.java
+++ pkiservice/src/main/java/io/tomahawkd/pki/dao/SystemApiDataDao.java
@@ -6,12 +6,14 @@ import org.apache.ibatis.annotations.*;
 @Mapper
 public interface SystemApiDataDao {
 
-	@Select("select system_index, system_api, register_date from system_api_index " +
-			"where system_index = #{id} limit 0,1")
+	@Select("select system_id, system_api, register_date, public_key, private_key from system_api_index " +
+			"where system_id = #{id} limit 0,1")
 	@Results({
-			@Result(property = "systemId", column = "system_index"),
+			@Result(property = "systemId", column = "system_id"),
 			@Result(property = "systemApi", column = "system_api"),
-			@Result(property = "registerDate", column = "register_date")
+			@Result(property = "registerDate", column = "register_date"),
+			@Result(property = "publicKey", column = "public_key"),
+			@Result(property = "privateKey", column = "private_key")
 	})
 	SystemApiDataModel getApiDataById(int id);
 
--- pkiservice/src/main/java/io/tomahawkd/pki/service/SystemApiDataService.java
+++ pkiservice/src/main/java/io/tomahawkd/pki/service/SystemApiDataService.java
@@ -1,5 +1,6 @@
 package io.tomahawkd.pki.service;
 
+import io.tomahawkd.pki.exceptions.CipherErrorException;
 import io.tomahawkd.pki.model.SystemApiDataModel;
 
 public interface SystemApiDataService {
@@ -7,7 +8,7 @@ public interface SystemApiDataService {
 	SystemApiDataModel getApiById(int systemId);
 
 	// return system id
-	int registerSystemApi();
+	int registerSystemApi() throws CipherErrorException;
 
 	boolean checkApi(SystemApiDataModel data, String systemApi);
 }
