9c2b25911990846dbf70936aa79c4d5ef2c43e7a
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-07-02 10:29:13 +0800

    sync with database

--- pkiservice/src/main/java/io/tomahawkd/pki/dao/SystemUserDao.java
+++ pkiservice/src/main/java/io/tomahawkd/pki/dao/SystemUserDao.java
@@ -6,12 +6,18 @@ import org.apache.ibatis.annotations.*;
 @Mapper
 public interface SystemUserDao {
 
-	@Insert("insert into system_user (`username`, `password`) values (#{user}, #{pass})")
+	@Insert("insert into system_user (`username`, `password`) values (#{user.username}, #{user.password})")
 	@Options(keyProperty = "userId", useGeneratedKeys = true)
 	int addSystemUser(@Param("user") SystemUserModel user);
 
+
 	@Select("select `system_user_id`, `username`, `password` " +
 			"from system_user where username = #{user.username} and password = #{user.password}")
+	@Results({
+			@Result(property = "userId", column = "system_user_id"),
+			@Result(property = "username", column = "username"),
+			@Result(property = "password", column = "password")
+	})
 	SystemUserModel getSystemUserByUsername(@Param("user") SystemUserModel user);
 
 }
