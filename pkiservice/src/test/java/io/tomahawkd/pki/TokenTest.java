4976cef2640bba555a909ee5a392d446a1cdc72b
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-07-02 14:19:06 +0800

    update test

--- pkiservice/src/test/java/io/tomahawkd/pki/TokenTest.java
+++ pkiservice/src/test/java/io/tomahawkd/pki/TokenTest.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.security.KeyPair;
