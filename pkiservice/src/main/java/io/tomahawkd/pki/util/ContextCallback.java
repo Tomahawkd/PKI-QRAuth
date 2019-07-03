d52b4bad8ce983a3bf51972f3bed0fce281fdccb
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-07-03 18:50:16 +0800

    add exception

--- pkiservice/src/main/java/io/tomahawkd/pki/util/ContextCallback.java
+++ pkiservice/src/main/java/io/tomahawkd/pki/util/ContextCallback.java
@@ -1,7 +1,9 @@
 package io.tomahawkd.pki.util;
 
+import java.io.IOException;
+
 @FunctionalInterface
 public interface ContextCallback<L, M, N, O, P, R> {
 
-	R invoke(L l, M m, N n, O o, P p) throws RuntimeException;
+	R invoke(L l, M m, N n, O o, P p) throws RuntimeException, IOException;
 }
