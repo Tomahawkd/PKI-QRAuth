48df63a3dc3078a7904d6d2d626a310b12ebc802
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-07-03 18:50:42 +0800

    implement qr service

--- pkiservice/src/main/java/io/tomahawkd/pki/service/QrStatusService.java
+++ pkiservice/src/main/java/io/tomahawkd/pki/service/QrStatusService.java
@@ -5,4 +5,12 @@ import io.tomahawkd.pki.model.QrStatusModel;
 public interface QrStatusService {
 
 	QrStatusModel generateQrNonce(String symKey, String iv);
+
+	void updateQrNonceStatusToScanned(int tokenId, int nonce);
+
+	void updateQrNonceStatusToConfirmed(int tokenId);
+
+	void clearStatus(int tokenId);
+
+	QrStatusModel getQrStatusByNonce(int nonce);
 }
