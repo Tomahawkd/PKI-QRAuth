1de60842814a7b390a30c0b089715a19f558eddd
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-07-04 16:19:47 +0800

    update token model construction, delete token and nonce update

--- pkiservice/src/main/java/io/tomahawkd/pki/service/UserTokenService.java
+++ pkiservice/src/main/java/io/tomahawkd/pki/service/UserTokenService.java
@@ -2,6 +2,10 @@ package io.tomahawkd.pki.service;
 
 import io.tomahawkd.pki.model.TokenModel;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 public interface UserTokenService {
 
 	TokenModel generateNewToken(String userTag, int systemId, String device, String ip);
@@ -11,4 +15,8 @@ public interface UserTokenService {
 	TokenModel getTokenById(int tokenId);
 
 	boolean validateToken(TokenModel token, int nonce);
+
+	List<Map<String, String>> getTokenListByUserId(int userId) throws IOException;
+
+	int deleteUserTokenById(int tokenId, int userId);
 }
