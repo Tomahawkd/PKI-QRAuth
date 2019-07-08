4bf239923eb0ed36ea76e22a7b823ba0131b7cc2
Dracula1998 <1446953337@qq.com>
2019-07-08 10:33:55 +0800

    add the update password function, untested

--- web/src/main/java/io/tomahawkd/simpleserver/controller/UserInfoController.java
+++ web/src/main/java/io/tomahawkd/simpleserver/controller/UserInfoController.java
@@ -38,7 +38,7 @@ public class UserInfoController {
 
 
     @GetMapping("/data")
-    public String getInfoPageById(HttpServletRequest request)  {
+    public String getInfoPageById(HttpServletRequest request) {
 
         int userid = (int) request.getSession().getAttribute("userid");
 
@@ -98,7 +98,7 @@ public class UserInfoController {
 
             }
         } catch (Exception e) {
-            throw  new MalformedJsonException("Json parse error");
+            throw new MalformedJsonException("Json parse error");
         }
     }
 
@@ -132,7 +132,7 @@ public class UserInfoController {
 
             }
         } catch (Exception e) {
-            throw  new MalformedJsonException("Json parse error");
+            throw new MalformedJsonException("Json parse error");
         }
     }
 
