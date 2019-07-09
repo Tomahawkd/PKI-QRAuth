fc95d8d5c2f1751e3be0e4fcf268f83231e9c25e
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-07-09 16:16:23 +0800

    return message as string

--- pkiservice/src/main/java/io/tomahawkd/pki/exceptions/ControllerExceptionHandler.java
+++ pkiservice/src/main/java/io/tomahawkd/pki/exceptions/ControllerExceptionHandler.java
@@ -17,14 +17,14 @@ public class ControllerExceptionHandler {
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MalformedJsonException.class)
 	@ResponseBody
-	public Map<String, Object> malformedJson(Exception e) {
+	public Map<String, String> malformedJson(Exception e) {
 		return handle(e);
 	}
 
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	@ExceptionHandler({NullPointerException.class, NotFoundException.class})
 	@ResponseBody
-	public Map<String, Object> notFound(Exception e) {
+	public Map<String, String> notFound(Exception e) {
 		return handle(e);
 
 	}
@@ -32,7 +32,7 @@ public class ControllerExceptionHandler {
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler(CipherErrorException.class)
 	@ResponseBody
-	public Map<String, Object> cipherIssue(Exception e) {
+	public Map<String, String> cipherIssue(Exception e) {
 		return handle(e);
 
 	}
@@ -40,7 +40,7 @@ public class ControllerExceptionHandler {
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(Base64EncodeException.class)
 	@ResponseBody
-	public Map<String, Object> base64Issue(Exception e) {
+	public Map<String, String> base64Issue(Exception e) {
 		return handle(e);
 
 	}
@@ -48,17 +48,17 @@ public class ControllerExceptionHandler {
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler(Exception.class)
 	@ResponseBody
-	public Map<String, Object> others(Exception e) {
+	public Map<String, String> others(Exception e) {
 		e.printStackTrace();
 		return handle(new Exception("Internal Error"));
 	}
 
-	private Map<String, Object> handle(Exception e) {
+	private Map<String, String> handle(Exception e) {
 		String tResponse = ThreadContext.getContext().get();
 		Message<String> message = new Message<String>().setError().setMessage(e.getMessage());
-		Map<String, Object> response = new HashMap<>();
+		Map<String, String> response = new HashMap<>();
 		if (tResponse != null && !tResponse.isEmpty()) response.put("T", tResponse);
-		response.put("M", message);
+		response.put("M", message.toJson());
 		return response;
 	}
 }
