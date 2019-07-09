c94d238292e5f7d4308da6df73b87aebeadc8267
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-07-09 18:10:21 +0800

    fix sql

--- pkiservice/src/main/java/io/tomahawkd/pki/dao/UserIndexDao.java
+++ pkiservice/src/main/java/io/tomahawkd/pki/dao/UserIndexDao.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface UserIndexDao {
 
-	@Select("select user_id from user_id_tag where user_tag = #{tag} and #{system} limit 0,1")
+	@Select("select user_id from user_id_tag where user_tag = #{tag} and system_id = #{system} limit 0,1")
 	int getUserIdByTag(String tag, int system);
 
 	@Select("select user_tag from user_id_tag where user_id = #{id} limit 0,1")
